9736b0c4985ad0c640dc601bb99d71fc77e6ddd7
bibashmanjusubedi <acharabibash@outlook.com>
2025-02-28 10:13:48 +0545

    Functions

---
+++ Functions/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int square(int num)
+{
+    int sq = num * num;
+    return sq;
+}
+
+Console.WriteLine(square(3));
