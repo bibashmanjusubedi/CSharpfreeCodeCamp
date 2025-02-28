ad94c984f1e957c3b66cdbd78fe05d733ef8b6d4
bibashmanjusubedi <acharabibash@outlook.com>
2025-02-28 10:42:58 +0545

    Object Oriented Programming(OOP) Concepts
    atrributes, methods and constructors

---
+++ OOP/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+var bibash = new Person(26, "Bibash Acharya", "1999 Feb 7");
+Console.WriteLine(bibash.name); // Output: Bibash Acharya
+Console.WriteLine("Square of age: " + bibash.SquareOfAge()); // Output: Square of age: 676
+
+class Person
+{
+    public int age;
+    public string name;
+    public string birthday;
+
+    public Person(int umer, string naam, string janmadin)
+    {
+        age = umer;
+        name = naam;
+        birthday = janmadin;
+    }
+
+    // Method to calculate the square of age
+    public int SquareOfAge()
+    {
+        return age * age;
+    }
+}
