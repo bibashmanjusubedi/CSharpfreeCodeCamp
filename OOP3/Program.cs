f15cca66871c99093f7bfe7037a8bd252e7c83f1
bibashmanjusubedi <acharabibash@outlook.com>
2025-03-11 15:19:03 +0545

    OOP3

---
+++ OOP3/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// class Car
+// {
+//     string color = "red";
+
+//     static void Main(string[] args)
+//     {
+//         Car myObj  = new Car();
+//         Car myObj1  = new Car();
+//         Car myObj2 = new Car();
+//         Console.WriteLine(myObj.color);
+//         Console.WriteLine(myObj1.color);
+//         Console.WriteLine(myObj2.color);
+//     }
+// }
+
+using System;
+
+namespace MyApplication
+{
+  class Car
+  {
+    string color = "red";
+
+    static void Main(string[] args)
+    {
+      Car myObj1 = new Car();
+      Car myObj2 = new Car();
+      Console.WriteLine(myObj1.color);
+      Console.WriteLine(myObj2.color);
+    }
+  }
+}
